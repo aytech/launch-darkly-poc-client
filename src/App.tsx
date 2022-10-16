dccd49d0d9bf4e4daf8b3b561679f20c22bb3506
Oleg <Yapparov>
2022-10-16 22:06:12 +0200

    Build simple UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Alert, Layout, Typography } from 'antd'
+import { Content } from 'antd/lib/layout/layout'
+import { useFlags } from 'launchdarkly-react-client-sdk'
+import { useEffect, useState } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+  ldClientAvailable: boolean
 }
 
-export default App;
+export default function App({ ldClientAvailable }: Props) {
+
+  const { demoFeatureToggle } = useFlags()
+
+  const [reactDemoFlag, setReactDemoFlag] = useState<boolean>(false)
+  const [springDemoFlag, setSpringDemoFlag] = useState<boolean>(false)
+
+  const { Title } = Typography
+
+  const reactSDKElement = () => {
+    if (ldClientAvailable === true) {
+      return (
+        <>
+          <Alert message="React SDK is enabled" type="success" showIcon />
+          <Alert
+            message={ `Demo Feature Toggle is ${demoFeatureToggle === true ? 'enabled' : 'disabled'}` }
+            type={ `${demoFeatureToggle === true ? 'success' : 'error'}` }
+            showIcon />
+        </>
+      )
+    }
+    return <Alert message="React SDK is disabled" type="error" showIcon />
+  }
+
+  const reactFlagElement = () => {
+    return reactDemoFlag === true ? (
+      <Alert message="React Demo Flag is enabled" type="success" showIcon />
+    ) : (
+      <Alert message="React Demo Flag is disabled" type="error" showIcon />
+    )
+  }
+
+  const springFlagElement = () => {
+    return springDemoFlag === true ? (
+      <Alert message="Spring Demo Flag is enabled" type="success" showIcon />
+    ) : (
+      <Alert message="Spring Demo Flag is disabled" type="error" showIcon />
+    )
+  }
+
+  useEffect(() => {
+    fetch("/flags")
+      .then(response => response.json())
+      .then((result: { reactDemoFlag: boolean, springDemoFlag: boolean }) => {
+        setReactDemoFlag(result.reactDemoFlag)
+        setSpringDemoFlag(result.springDemoFlag)
+      })
+  })
+
+  return (
+    <Layout>
+      <Content>
+        <Title>LaunchDarkly barebones demo</Title>
+        { reactSDKElement() }
+        { reactFlagElement() }
+        { springFlagElement() }
+      </Content>
+    </Layout>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
+import { asyncWithLDProvider, LDProvider } from 'launchdarkly-react-client-sdk'
+import App from './App'
+import 'antd/dist/antd.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const mainElement = async (clientId: string | null) => {
+  if (clientId === null) return <App ldClientAvailable={ false } />
+  const LDProvider = await asyncWithLDProvider({
+    clientSideID: clientId
+  })
+  return (
+    <LDProvider>
+      <App ldClientAvailable={ true } />
+    </LDProvider>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+(async () => {
+  // Set clientSideID to your own Client-side ID. You can find this in
+  // your LaunchDarkly portal under Account settings / Projects
+  const configuration = await fetch("/configuration")
+  const { ldClientId } = await configuration.json()
+  const element = await mainElement(ldClientId)
+  createRoot(document.getElementById("root")!).render(element)
+})();
